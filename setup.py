0a716b9d17affd837bec844093d6c94dec0b0f6a
Joeri Jongbloets <j.a.jongbloets@uva.nl>
2017-01-17 18:43:01 +0100

    change to MIT license

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Topic :: Scientific/Engineering',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
